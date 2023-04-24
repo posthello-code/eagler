a3b3d044627ffc91cddfaea26bb030e7f0c1b547
Alex <github@alexjensen.net>
2023-04-24 01:28:59 -0700

    cleanup

--- lib/services/response_extractor.dart
+++ lib/services/response_extractor.dart
@@ -9,48 +9,37 @@ dynamic extractValueFromResponse(Response response, String path) {
 
 // function to parse the body recursively
 dynamic traverseJsonObject(dynamic body, String path) {
-  print(body);
-  print(path);
   List<String> properties = path.split('.');
 
   if (properties.length == 1) {
     if (properties[0].contains('body[')) {
-      // return selected item when the path is body[i]
+      // get the index of the item in the body[i] array
       int index = int.parse(properties[0].split('[')[1].split(']')[0]);
       return jsonDecode(body)[index];
-    } else if (properties[0].contains('[')) {
-      // return the selected item of array[i] when there's only one property left
-      return body;
-    } else if (properties[0] == 'first') {
-      // return first item in "values"
-      return body;
-    } else if (body is num) {
-      return body;
-    } else if (body[0] == '{' && properties[0] != 'body') {
-      // return objects when there's only one property left
+    } else if (body is String && body[0] == '{' && properties[0] != 'body') {
+      // return objects when there's only one property left in the path
       return jsonDecode(body);
-    } else if (properties[0] != 'body') {
-      // return value when there's only one property left
-      return body;
     } else {
-      // default also handles when path == 'body'
+      // return the value of the last property in the path
       return body;
     }
-  } else {
+  }
+  // handle when there is more than one property in the path
+  else {
     // necessary to remove first property if calling traversing JSON recursively
     path = removeFirstPropertyFromPath(path);
     if (properties[0].contains('[') && properties[1].contains('[')) {
-      // handle body[]
+      // handle whent the next two properties are lists
       int index = int.parse(properties[0].split('[')[1].split(']')[0]);
       String nextProperty = properties[1].split('[')[0];
-      // find selected item in array, and return the next property in path
+      // find selected item in list, and return the next property in path
       Map selectedItem = jsonDecode(body)[index];
       body = selectedItem[nextProperty];
       return traverseJsonObject(body, path);
     } else if (properties[0].contains('[')) {
-      // handle body[]
+      // handle when the list is the first property in the path,
+      // and the next property is not a list
       int index = int.parse(properties[0].split('[')[1].split(']')[0]);
-
       if (body is String) {
         body = json.decode(body)[index][properties[1]];
       } else if (body is Map) {
@@ -58,35 +47,27 @@ dynamic traverseJsonObject(dynamic body, String path) {
       } else {
         body = body[index][properties[1]];
       }
-
       return traverseJsonObject(body, path);
     } else if (properties[1].contains('[')) {
-      // handle lists
+      // handle object property is a list
       String arrayProp = properties[1].split('[')[0];
       int index = int.parse(properties[1].split('[')[1].split(']')[0]);
       body = jsonDecode(body)[arrayProp][index];
-      if (body is int) {
-        body = body.toString();
-      }
       return traverseJsonObject(body, path);
     } else if (properties[1] == 'values') {
+      // special case for 'values' keyword
       return traverseJsonObject(body.values.toList(), path);
     } else if (properties[1] == 'first') {
+      // special case for 'first' keyword
       return traverseJsonObject(body.first, path);
+    } else if (body is Map<String, dynamic>) {
+      // handle when object property is a also an object
+      body = json.encode(body);
+      body = json.decode(body)[properties[1]];
+      return traverseJsonObject(body, path);
     } else {
-      body;
-      if (body is String) {
-        body = json.decode(body)[properties[1]];
-      } else {
-        body = body[properties[1]];
-      }
-
-      if (body is Map<String, dynamic> || body is List<dynamic>) {
-        body = json.encode(body);
-      }
-      if (body is int) {
-        body = body.toString();
-      }
+      // handle when object property is any other type
+      body = json.decode(body)[properties[1]];
       return traverseJsonObject(body, path);
     }
   }
--- test/services/extractor_test.dart
+++ test/services/extractor_test.dart
@@ -13,48 +13,64 @@ void main() {
       }
     ]);
   });
+
   test("successful parse an element of the body when its a list", () {
     Response res = Response('[{"entries":{"unknownProp": 1.74}}]', 200);
     expect(extractValueFromResponse(res, 'body[0]'), {
       "entries": {"unknownProp": 1.74}
     });
   });
-  test("successful parse properties of an item in list response", () {
+
+  test("successful parse properties of a list", () {
     Response res = Response('[{"subdeviceGuid":"1234"}]', 200);
 
     expect((jsonDecode(extractValueFromResponse(res, 'body[0].subdeviceGuid'))),
         1234);
   });
-  test("successful parse properties from an object response", () {
+
+  test("successful parse properties of objects", () {
     Response res = Response('{"entries":{"unknownProp": 1.74}}', 200);
     expect(
         extractValueFromResponse(res, 'body.entries'), {"unknownProp": 1.74});
   });
-  test("successful parse array strings from response object", () {
+
+  test("successful parse string values from list", () {
     Response res = Response(
         '{"entries":{"unknownProp": 1.74}, "another": [ "test", "test2" ]}',
         200);
     expect(extractValueFromResponse(res, 'body.another[1]'), "test2");
   });
-  test("successful parse array ints fro from response object", () {
+
+  test("successful parse int from a list", () {
     Response res = Response(
         '{"entries":{"unknownProp": 1.74}, "another": [ 12, 13 ]}', 200);
-    expect(int.parse(extractValueFromResponse(res, 'body.another[1]')), 13);
+    expect(extractValueFromResponse(res, 'body.another[1]'), 13);
   });
   test("successful parse 'values' keyword", () {
     Response res = Response('[{"entries":{"unknownProp": 1.74}}]', 200);
     expect(extractValueFromResponse(res, 'body[0].entries.values'), [1.74]);
   });
-  test(
-      "successfully parse properties of array items that are past body in the path",
-      () {
+
+  test("successful parse 'first' keyword", () {
+    Response res = Response('[{"entries":{"unknownProp": 1.74}}]', 200);
+    expect(extractValueFromResponse(res, 'body[0].entries.values.first'), 1.74);
+  });
+
+  test("successfully parse properties of list items", () {
     Response res = Response('{"entries":[{"API": 1.74}]}', 200);
     expect((extractValueFromResponse(res, 'body.entries[0].API')), 1.74);
   });
-  test(
-      "successfully parse properties of array items that are past body in the path",
-      () {
+
+  test("successfully parse lists of lists", () {
     Response res = Response('[{"entries":[{"API": 1.74}]}]', 200);
     expect((extractValueFromResponse(res, 'body[0].entries[0].API')), 1.74);
+  });
+
+//body.results[0].location.street
+  test("successfully parse lists of lists", () {
+    Response res = Response(
+        '{"results":[{"location":{"street":{"someObject":"test"}}}]}', 200);
+    expect((extractValueFromResponse(res, 'body.results[0].location.street')),
+        {"someObject": "test"});
   });
 }
